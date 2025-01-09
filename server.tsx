950c56be1ebcd416b56fb46b27e578bafe318ee9
yong <den9yon9@gmail.com>
2025-01-09 11:33:54 +0800

    fix: flow dispatch

--- server.tsx
+++ server.tsx
@@ -140,7 +140,7 @@ function Flows() {
           return (
             <form
               method="post"
-              action="/dispatch"
+              action="/flows/dispatch"
               key={`${item.origin}/${item.branch}`}
               className="shadow bg-base-100 rounded-2xl p-5 flex flex-col gap-2 border-primary"
             >
