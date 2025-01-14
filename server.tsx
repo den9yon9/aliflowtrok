8d0922b3c5bf7c2a6d40f1c56b6da681e5077cee
yong <den9yon9@gmail.com>
2025-01-14 11:44:07 +0800

    fix: hub

--- server.tsx
+++ server.tsx
@@ -1,7 +1,7 @@
 /** @jsxRuntime automatic */
 /** @jsxImportSource npm:preact@10.25.4 */
 
-import hub from "jsr:@trok/trok@0.1.42/hub";
+import hub from "jsr:@trok/trok@0.1.43/hub";
 import { basename } from "jsr:@std/path@^1.0.8";
 import { render } from "npm:preact-render-to-string@^6.5.12";
 import { extname, resolve } from "jsr:@std/path@1.0.8";
